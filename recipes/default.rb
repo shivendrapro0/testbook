96331803c92b642fee44bf7bacbc38c84149692e
Shivendra <shivendrapro0@gmail.com>
2018-06-28 04:42:23 +0000

    testing policyfile

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,5 +5,10 @@ log "Welcome to Chef, #{node["testbook"]["starter_name"]}! :version #{run_contex
 }" do
   level :info
 end
+log "We are testing chef Policyfile! :version #{run_context.cookbook_collection[cookbook_name].version
+}" do
+  level :info
+end
+
 
 # For more information, see the documentation: https://docs.chef.io/essentials_cookbook_recipes.html
